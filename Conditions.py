76a9cd7c8caf26e8f08f25993ecee773e4838c17
ryanhuitzil <98927204+ryanhuitzil@users.noreply.github.com>
2022-02-16 17:38:27 -0500

    conditions with temperature and clothing options

---
+++ Conditions.py
@@ -0,0 +1,32 @@
+temp = 90
+print ("Please enter the current temperature:")
+Please enter the current temperature:
+90
+print ("Wear shorts")
+Wear shorts
+       
+print ("Please enter the current temperature:")
+       
+Please enter the current temperature:
+75
+       
+print ("Please enter the current temperature:")
+       
+Please enter the current temperature:
+60
+       
+print ("Wear a jacket")
+       
+Wear a jacket
+print ("Please enter the current temperature:")
+       
+Please enter the current temperature:
+33
+       
+print ("Wear a heavy coat")
+       
+Wear a heavy coat
+       
+print ("Stay inside")
+       
+Stay inside
