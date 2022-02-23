c47bbfadaa0e2700b2efd80a81466d49672426aa
ryanhuitzil <98927204+ryanhuitzil@users.noreply.github.com>
2022-02-23 15:05:04 -0500

    Average.py

---
+++ Average.py
@@ -0,0 +1,35 @@
+average = 0
+total = 0
+howManyEntered = 0
+howMany = int(input('60'))
+howManyEntered = 1
+total = total + 50
+total = 1 + 50
+
+'while'
+'average = 0'
+'total = 0'
+'howManyEntered = 0'
+'howMany = int(input(60))'
+'howManyEntered = 2'
+'total = total + 60'
+'total = 2 + 60'
+
+'while'
+'average = 0'
+'total = 0'
+'howManyEntered = 0'
+'howMany = int(input(70))'
+'howManyEntered = 2'
+'total = total + 60'
+'total = 3 + 60'
+
+average = total / howMany
+
+print ('the average for the test scores you entered is')
+print('average')
+
+
+
+
+
